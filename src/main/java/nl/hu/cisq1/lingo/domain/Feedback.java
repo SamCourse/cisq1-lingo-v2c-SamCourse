bc2dfdab2843745b05394b43a978b96b0be8ad75
Sam Course <notcourseps@gmail.com>
2022-03-03 22:08:06 +0100

    Feedback instantiation solidify
    Added ways of creation of Feedback objects to make sure feedback throws an exception in case of mismatching lengths
    Added test to assert the creation exception for invalid cases

--- src/main/java/nl/hu/cisq1/lingo/domain/Feedback.java
+++ src/main/java/nl/hu/cisq1/lingo/domain/Feedback.java
@@ -1,17 +1,40 @@
 package nl.hu.cisq1.lingo.domain;
 
+import lombok.AccessLevel;
 import lombok.AllArgsConstructor;
 import lombok.EqualsAndHashCode;
 import lombok.ToString;
+import nl.hu.cisq1.lingo.domain.exception.InvalidFeedbackException;
 
+import java.util.Collections;
 import java.util.List;
 
-@AllArgsConstructor
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
 @EqualsAndHashCode
 @ToString
 public class Feedback {
     private String attempt;
     private List<Mark> marks;
+
+    public static Feedback create(String attempt, List<Mark> marks) {
+        if (attempt.length() != marks.size()) {
+            throw new InvalidFeedbackException();
+        }
+
+        return new Feedback(attempt, marks);
+    }
+
+    public static Feedback correct(String attempt) {
+        return new Feedback(attempt, Collections.nCopies(attempt.length(), Mark.CORRECT));
+    }
+
+    public static Feedback incorrect(String attempt) {
+        return new Feedback(attempt, Collections.nCopies(attempt.length(), Mark.ABSENT));
+    }
+
+    public static Feedback invalid(String attempt) {
+        return new Feedback(attempt, Collections.nCopies(attempt.length(), Mark.INVALID));
+    }
 
     public boolean isWordGuessed() {
         return marks.stream().allMatch(mark -> mark.equals(Mark.CORRECT));
--- src/test/java/nl/hu/cisq1/lingo/domain/FeedbackTest.java
+++ src/test/java/nl/hu/cisq1/lingo/domain/FeedbackTest.java
@@ -1,5 +1,6 @@
 package nl.hu.cisq1.lingo.domain;
 
+import nl.hu.cisq1.lingo.domain.exception.InvalidFeedbackException;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 
@@ -12,28 +13,36 @@ class FeedbackTest {
     @Test
     @DisplayName("word is guessed if all letters are correct")
     void isWordGuessed() {
-        Feedback feedback = new Feedback("woord", Arrays.asList(Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = Feedback.correct("woord");
         assertTrue(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("word is not guessed if not all letters are correct")
     void wordIsNotGuessed() {
-        Feedback feedback = new Feedback("woord", Arrays.asList(Mark.ABSENT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = Feedback.incorrect("woord");
         assertFalse(feedback.isWordGuessed());
     }
 
     @Test
     @DisplayName("guess is valid if none of the marks are invalid")
     void guessIsValid() {
-        Feedback feedback = new Feedback("woord", Arrays.asList(Mark.ABSENT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = Feedback.correct("woord");
         assertTrue(feedback.isGuessValid());
     }
 
     @Test
     @DisplayName("guess is invalid if any of the marks are invalid")
     void guessIsInvalid() {
-        Feedback feedback = new Feedback("woord", Arrays.asList(Mark.INVALID, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT));
+        Feedback feedback = Feedback.invalid("woord");
         assertFalse(feedback.isGuessValid());
+    }
+
+    @Test
+    @DisplayName("feedback throws exception if guess word length does not match feedback mark amount")
+    void feedbackThrowsExceptionForMismatchedLengths() {
+        assertThrows(InvalidFeedbackException.class,
+                () -> Feedback.create("woord", Arrays.asList(Mark.INVALID, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT, Mark.CORRECT)));
+
     }
 }
