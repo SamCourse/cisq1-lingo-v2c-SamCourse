e5621500b25b0f46e551ea136e24ff54f0ceea28
Sam Course <notcourseps@gmail.com>
2022-03-06 01:07:17 +0100

    Use Lombok for Word.java

--- src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
+++ src/main/java/nl/hu/cisq1/lingo/words/domain/Word.java
@@ -1,27 +1,23 @@
 package nl.hu.cisq1.lingo.words.domain;
 
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
 import javax.persistence.Column;
 import javax.persistence.Entity;
 import javax.persistence.Id;
 
 @Entity(name = "words")
+@NoArgsConstructor
+@Getter
 public class Word {
     @Id
     @Column(name = "word")
     private String value;
     private Integer length;
 
-    public Word() {}
     public Word(String word) {
         this.value = word;
         this.length = word.length();
-    }
-
-    public String getValue() {
-        return value;
-    }
-
-    public Integer getLength() {
-        return length;
     }
 }
