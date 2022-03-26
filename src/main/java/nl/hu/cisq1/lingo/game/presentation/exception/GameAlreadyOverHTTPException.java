ca100d991ac374fe3bb1f23cd9540a29e6387354
Sam Course <notcourseps@gmail.com>
2022-03-26 10:52:32 +0100

    Added custom HTTP exceptions

---
+++ src/main/java/nl/hu/cisq1/lingo/game/presentation/exception/GameAlreadyOverHTTPException.java
@@ -0,0 +1,12 @@
+package nl.hu.cisq1.lingo.game.presentation.exception;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason="Game already ended")
+public class GameAlreadyOverHTTPException extends RuntimeException {
+    public GameAlreadyOverHTTPException() {
+        super("This game has already ended.");
+    }
+}
+
