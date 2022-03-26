56a7a2a28cec13f21c81a5a5715e5f14639585b7
Sam Course <notcourseps@gmail.com>
2022-03-26 11:04:07 +0100

    Added 3 new layers to Game component
    Added service, repository and controller for Game component

---
+++ src/main/java/nl/hu/cisq1/lingo/game/application/GameService.java
@@ -0,0 +1,77 @@
+package nl.hu.cisq1.lingo.game.application;
+
+import lombok.AllArgsConstructor;
+import nl.hu.cisq1.lingo.game.application.exception.GameNotFoundException;
+import nl.hu.cisq1.lingo.game.data.GameRepository;
+import nl.hu.cisq1.lingo.game.domain.Game;
+import nl.hu.cisq1.lingo.game.domain.exception.GameAlreadyOverException;
+import nl.hu.cisq1.lingo.round.domain.Round;
+import nl.hu.cisq1.lingo.words.application.WordService;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.UUID;
+
+@Service
+@Transactional
+@AllArgsConstructor
+public class GameService {
+    private WordService wordService;
+    private GameRepository gameRepository;
+
+    public Game startNewGame() {
+        String word = wordService.provideRandomWord(5);
+        Game game = new Game();
+        game.initializeRound(word);
+        gameRepository.save(game);
+        return game;
+    }
+
+    public Game guess(UUID gameId, String word) {
+        Optional<Game> optionalGame = gameRepository.findById(gameId);
+
+        if (optionalGame.isEmpty()) {
+            throw new GameNotFoundException(gameId);
+        }
+
+        Game game = optionalGame.get();
+
+        if (game.isOver()) {
+            throw new GameAlreadyOverException();
+        }
+
+        int currentRoundWordLength = game.getLastRound().getWordLength();
+
+        Round round = game.getLastRound();
+
+        if (word.length() > currentRoundWordLength) {
+            word = word.substring(0, currentRoundWordLength - 1);
+        }
+
+        boolean invalid = !wordService.wordExists(word) || word.length() < currentRoundWordLength;
+
+        round.guess(word, invalid);
+
+        if (round.hasEnded()) {
+            game.completeRound();
+
+            return startNewRound(gameId, game.getNextRoundWordLength());
+        }
+
+        return game;
+    }
+
+    public Game startNewRound(UUID gameId, int wordLength) {
+        Game game = gameRepository.getById(gameId);
+        String word = wordService.provideRandomWord(wordLength);
+        game.initializeRound(word);
+        return game;
+    }
+
+    public List<Game> getAllGames() {
+        return gameRepository.findAll();
+    }
+
+}
---
+++ src/main/java/nl/hu/cisq1/lingo/game/presentation/GameController.java
@@ -0,0 +1,56 @@
+package nl.hu.cisq1.lingo.game.presentation;
+
+import lombok.AllArgsConstructor;
+import nl.hu.cisq1.lingo.game.application.GameService;
+import nl.hu.cisq1.lingo.game.application.exception.GameNotFoundException;
+import nl.hu.cisq1.lingo.game.domain.Game;
+import nl.hu.cisq1.lingo.game.domain.exception.GameAlreadyOverException;
+import nl.hu.cisq1.lingo.game.presentation.dto.GameResponseDTO;
+import nl.hu.cisq1.lingo.game.presentation.exception.GameAlreadyOverHTTPException;
+import nl.hu.cisq1.lingo.game.presentation.exception.GameNotFoundHTTPException;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.UUID;
+
+@RestController
+@RequestMapping("/game")
+@AllArgsConstructor
+public class GameController {
+    private GameService gameService;
+
+    @GetMapping("/start")
+    public GameResponseDTO startGame() {
+        Game game = gameService.startNewGame();
+        return new GameResponseDTO(game);
+    }
+
+    @GetMapping("/list")
+    public List<GameResponseDTO> getGames() {
+        List<Game> games = gameService.getAllGames();
+        List<GameResponseDTO> gameDTOs = new ArrayList<>();
+        games.forEach(game -> gameDTOs.add(new GameResponseDTO(game)));
+
+        return gameDTOs;
+    }
+
+    @GetMapping("/guess")
+    public GameResponseDTO guess(@RequestParam(name = "game") UUID gameId,
+                                 @RequestParam(name = "guess") String guess) {
+        Game game;
+
+        try {
+            game = gameService.guess(gameId, guess);
+        } catch (GameNotFoundException e) {
+            throw new GameNotFoundHTTPException(gameId);
+        } catch (GameAlreadyOverException e) {
+            throw new GameAlreadyOverHTTPException();
+        }
+
+        return new GameResponseDTO(game);
+    }
+}
