cc06c43cfb551b806ce9a17d939920716a4ca882
Sam Course <notcourseps@gmail.com>
2022-03-27 13:05:47 +0200

    GameAlreadyOverHTTPException now throws FORBIDDEN instead of NOT_ACCEPTABLE

--- src/main/java/nl/hu/cisq1/lingo/game/presentation/exception/GameAlreadyOverHTTPException.java
+++ src/main/java/nl/hu/cisq1/lingo/game/presentation/exception/GameAlreadyOverHTTPException.java
@@ -3,7 +3,7 @@ package nl.hu.cisq1.lingo.game.presentation.exception;
 import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
-@ResponseStatus(value = HttpStatus.NOT_ACCEPTABLE, reason="Game already ended")
+@ResponseStatus(value = HttpStatus.FORBIDDEN, reason="Game already ended")
 public class GameAlreadyOverHTTPException extends RuntimeException {
     public GameAlreadyOverHTTPException() {
         super("This game has already ended.");
